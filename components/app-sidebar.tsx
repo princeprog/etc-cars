81f4799aac23e3419df2d79222b4482dc5f1dfbd
VincentPaul434 <shadowvator@gmail.com>
2026-06-27 23:20:13 +0800

    activity history relocated
    moved the activity history to admin section

--- components/app-sidebar.tsx
+++ components/app-sidebar.tsx
@@ -35,11 +35,6 @@ const data = {
       icon: <LayoutDashboardIcon />,
     },
     {
-      title: "Activity History",
-      url: "/activity-history",
-      icon: <ClipboardListIcon />,
-    },
-    {
       title: "Vehicles",
       url: "/vehicles",
       icon: <CarFrontIcon />,
@@ -71,6 +66,11 @@ const data = {
     },
   ],
   adminNav: [
+    {
+      title: "Activity History",
+      url: "/activity-history",
+      icon: <ClipboardListIcon />,
+    },
     {
       title: "Staff",
       url: "/staff",
